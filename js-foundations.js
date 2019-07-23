4d519cea9354a3c39bc5f9c4723b35a847fb1e15
Sarah Proctor <sproctor950@gmail.com>
2019-07-22 17:28:01 -0700

    updates to assessment, work in progress

--- js-foundations.js
+++ js-foundations.js
@@ -1,19 +1,45 @@
-// Javascript Foundations Assessments
+// ASSESSMENT 2: JAVASCRIPT FOUNDATIONS
+// Coding practical questions
 
-// 1. Write an anonymous function that takes one argument of type number and decides if that number is evenly divisble by three or not. If it is, print the number and "is divisible by three" or, print the number and "is not divisble by three".
+// 1. Write an arrow function that takes one argument of type number and decides if the number is evenly divisble by three or not. Use the test cases 15, 0, 385, -7. Expected output: "15 is divisible by three", "0 is divisible by three", "385 is not divisble by three", "-7 is not divisble by three".
 
-// 2. Write an object called helloMe. Include your first_name, last_name and at least two other properties of you. Write a function that returns a description of you.
 
-// 3. Create an array of 5 grocery items. Write a function that returns the first, middle, and last item as a string.
 
-// 4. Write a function called alphabetSoup that takes an argument of "str" and return a string with the letters in alphabetical order (ie. learn becomes aelnr). Assume numbers and punctuation symbols will not be included in the parameter.
+// 2. Create an object called helloMe. Include your firstName, lastName and TWO OTHER properties of you. Add a method to your object that will return a sentence describing you.
 
-// 5. Given the arrays below, write a function animalNums that uses a for loop to print one value from each array concatenated together.
 
+
+// -------------------------------------------------
+// Consider the variable:
+
+var randomNouns = ["streetlamp", "potato", "teeth", "conclusion", "nephew", "temperature", "database"]
+
+// 3a. Write a function that returns every other item as one string. Expected output: "streetlamp teeth nephew database"
+
+
+
+// 3b. Create an arrow function that takes in the variable and returns an array with the number of letters in each string. Expected output: [10, 6, 5, 10, 6, 11, 8]
+
+
+
+// 3c. Stretch: Create a function that takes in the variable and returns the array with all the words capitalized. Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew", "Temperature", "Database"]
+
+
+
+// -------------------------------------------------
+// Consider the variables:
+var testString1 = "learn"
+var testString2 = "academy"
+var testString3 = "sandiego"
+
+// 4a. Write a function called alphabetSoup that takes one argument and return a string with all the letters in alphabetical order. Use each of the varibales as test cases. Expected output: "aelnr" "aacdemy" "adeginos"
+
+
+
+
+// -------------------------------------------------
+// Consider the variables:
 var nums = [9, 5, 88, 2, 5, 42, 57]
-
 var nouns = ["ducks", "elephants", "pangolin", "rhinoceros", "giraffes", "penguins", "llamas"]
 
-// output should be: "9 ducks", etc
-
-//6. Create an array of 5 numbers. Write a function that loops through the array and returns a new array of the numbers multiplied by 5.
+// 5. Given the arrays below, write a function that returns one value from each array concatenated together. Expected output: "9 ducks" etc
